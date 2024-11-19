3faef52dd25f5995332d70d3838b76845bbce932
daniel92guerrero <89554125+daniel92guerrero@users.noreply.github.com>
2024-11-19 03:21:26 +0000

    update index.html

--- script.js
+++ script.js
@@ -26,29 +26,6 @@ function sendRSVP() {
     }
 }
 
-function startCountdown() {
-    console.log("Countdown function initiated");
-    const eventDate = new Date("October 4, 2025 17:00:00").getTime();
-
-    const countdownInterval = setInterval(() => {
-        const now = new Date().getTime();
-        const timeRemaining = eventDate - now;
-
-        const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
-        const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
-        const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
-
-        document.getElementById("time-remaining").innerHTML = `
-            ${days} Dias ${hours} Horas ${minutes} Minutos ${seconds} Segundos
-        `;
-
-        if (timeRemaining < 0) {
-            clearInterval(countdownInterval);
-            document.getElementById("time-remaining").innerHTML = "¡El día ha llegado!";
-        }
-    }, 1000);
-}
 
 document.getElementById('submit').addEventListener('click', function (event) {
     event.preventDefault();
@@ -78,5 +55,31 @@ document.getElementById('submit').addEventListener('click', function (event) {
     .catch(error => console.error('Error:', error));
 });
 
+
+
+
+function startCountdown() {
+    console.log("Countdown function initiated");
+    const eventDate = new Date("October 4, 2025 17:00:00").getTime();
+
+    const countdownInterval = setInterval(() => {
+        const now = new Date().getTime();
+        const timeRemaining = eventDate - now;
+
+        const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+
+        document.getElementById("time-remaining").innerHTML = `
+            ${days} Dias ${hours} Horas ${minutes} Minutos ${seconds} Segundos
+        `;
+
+        if (timeRemaining < 0) {
+            clearInterval(countdownInterval);
+            document.getElementById("time-remaining").innerHTML = "¡El día ha llegado!";
+        }
+    }, 1000);
+}
 // Start the countdown on page load
 window.onload = startCountdown;
